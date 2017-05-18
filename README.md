a6eca19d59663999dc384a027cff3e3e38250f82
Your Full Name <youremailaddress@example.com>
2017-05-18 13:06:35 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# hw4-megaroster
+# Megaroster
+
+## Day 3 Homework
+
+Finish yesterday's homework, using my Megaroster as a base. Don't forget to keep the `students` array in sync.
+
+### Bonus Credit
+
+Fool around with Foundation and/or CSS to make it look nicer.
+
+### Mega Bonus Credit
+
+Add buttons to change the position of the student within the list. In other words, add "Move Up" and "Move Down" buttons.
+
+### Super Mega Bonus Credit
+
+Persist the student data using `window.localStorage`.
+
+## Day 4 Homework
+
+Make "move down" work if you haven't already.
+
+Allow the editing of student names after they're already in the list. (Gee, it would be nice if I could make that span's _content editable_!)
+
+### Super Mega Bonus Credit Hyper Fighting
+
+Have a nice weekend.
---
+++ js/app.js
@@ -0,0 +1,142 @@
+$(document).foundation()
+
+class Megaroster {
+  constructor(listSelector) {
+    this.studentList = document.querySelector(listSelector)
+    this.students = []
+    this.max = 0
+    this.setupEventListeners()
+    this.load()
+  }
+
+  setupEventListeners() {
+    document
+      .querySelector('#new-student')
+      .addEventListener('submit', this.addStudentViaForm.bind(this))
+  }
+
+  save() {
+    localStorage.setItem('roster', JSON.stringify(this.students))
+  }
+
+  load() {
+    const rosterString = localStorage.getItem('roster')
+    if (rosterString) {
+      const rosterArray = JSON.parse(rosterString)
+      rosterArray
+        .reverse()
+        .map(this.addStudent.bind(this))
+    }
+  }
+
+  removeStudent(ev) {
+    const btn = ev.target
+    const li = btn.closest('.student')
+
+    for (let i=0; i < this.students.length; i++) {
+      let currentId = this.students[i].id.toString()
+      if (currentId === li.dataset.id) {
+        this.students.splice(i, 1)
+        break
+      }
+    }
+
+    li.remove()
+    this.save()
+  }
+
+  promoteStudent(student, ev) {
+    const btn = ev.target
+    const li = btn.closest('.student')
+    student.promoted = !student.promoted
+
+    if (student.promoted) {
+      li.classList.add('promoted')
+    } else {
+      li.classList.remove('promoted')
+    }
+    
+    this.save()
+  }
+
+  addStudentViaForm(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const student = {
+      id: this.max + 1,
+      name: f.studentName.value,
+    }
+    this.addStudent(student)
+    f.reset()
+  }
+
+  addStudent(student, append) {
+    this.students.unshift(student)
+
+    const listItem = this.buildListItem(student)
+    this.prependChild(this.studentList, listItem)
+
+    if (student.id > this.max) {
+      this.max = student.id
+    }
+    this.save()
+  }
+
+  prependChild(parent, child) {
+    parent.insertBefore(child, parent.firstChild)
+  }
+
+  buildListItem(student) {
+    const template = document.querySelector('.student.template')
+    const li = template.cloneNode(true)
+    this.removeClassName(li, 'template')
+    li.querySelector('.student-name').textContent = student.name
+    li.dataset.id = student.id
+
+    if(student.promoted) {
+      li.classList.add('promoted')
+    }
+
+    this.setupActions(li, student)
+    return li
+  }
+
+  setupActions(li, student) {
+    li
+      .querySelector('button.remove')
+      .addEventListener('click', this.removeStudent.bind(this))
+
+    li
+      .querySelector('button.promote')
+      .addEventListener('click', this.promoteStudent.bind(this, student))
+
+    li
+      .querySelector('button.move-up')
+      .addEventListener('click', this.moveUp.bind(this, student))
+
+  }
+
+  moveUp(student, ev) {
+    const btn = ev.target
+    const li = btn.closest('.student')
+
+    const index = this.students.findIndex((currentStudent, i) => {
+      return currentStudent.id === student.id
+    })
+
+    if (index > 0) {
+      this.studentList.insertBefore(li, li.previousElementSibling)
+
+      const previousStudent = this.students[index - 1]
+      this.students[index - 1] = student
+      this.students[index] = previousStudent
+
+      this.save()
+    }
+  }
+
+  removeClassName(el, className){
+    el.className = el.className.replace(className, '').trim()
+  }
+}
+const roster = new Megaroster('#studentList')
